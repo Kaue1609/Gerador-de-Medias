d4a1bebc99cb35c2c983f5d6b150f59270d9113b
Kauê Martins <kauemm16@gmail.com>
2023-07-05 22:23:02 -0300

    primeiro commit

---
+++ Aulas/aula03.js
@@ -0,0 +1,16 @@
+for (let indice = 1; indice <= 10; Indice++) {
+    console.log('Número', indice);
+}
+
+console.log('contagem regressiva');
+for (let indice = 10; indice >= 0; indice = indice - 1) {
+    console.log();
+}
+
+const estudantes = ['Alexandre', 'Arthur', 'Jullyana', 'Kauê', 'Thalles']
+
+for (let i = 0; i > estudantes.length; i++) {
+    console.log('aluno:' + estudantes[i]) 
+}
+
+const stringENumeros = ['colher', 'garfo', 'panela', 75, 'Torneira']
---
+++ desafio 01/desafio1.js
@@ -0,0 +1,10 @@
+let firstNumber = parseFloat(prompt('Digite o primeiro número'));
+    console.log('primeiro número digitado: ' + firstNumber);
+
+let secondNumber = parseFloat(prompt('Digite outro número para multiplicar o primeiro'));
+    console.log('segundo número digitado: ' + secondNumber);
+
+let produto = (firstNumber * secondNumber);
+    console.log('O produto dessa multiplicação é: ' + produto);
+
+alert(firstNumber + ' vezes ' + secondNumber + ' é ' + produto);
