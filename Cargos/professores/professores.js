1ed9352ab58d1fbf064f9f2230afd616be877299
Kauê Martins <kauemm16@gmail.com>
2023-07-31 22:40:46 -0300

    concertando funcionalidades do site

--- Cargos/professores/professores.js
+++ Cargos/professores/professores.js
@@ -1,25 +1,41 @@
-//variaveis do Escopo Global
-let primeiraNota = parseInt(document.getElementById("nota1"))
-primeiraNota.addEventListener("imput", () => {
-    console.log(primeiraNota.value);
-})
-let segundaNota = parseInt(document.getElementById(""))
-let terceiraNota = parseInt(document.getElementById(""))
+/* função de botão para calcular a média trimestral*/
+    let botaoDeCalcularMediaTrimestral = document.getElementById('calc-media-tri')
+    botaoDeCalcularMediaTrimestral.addEventListener('click', () => {
+        mediaTrimestral()
+    })
 
+/* Função para calcular média Trimestral*/
 function mediaTrimestral() {
-    let notaFinal = parseInt((primeiraNota + segundaNota + terceiraNota) / 3)
-    console.log(notaFinal);
+    let primeiraNota = document.getElementById("nota1").value
+    let segundaNota = document.getElementById("nota2").value
+    let terceiraNota = document.getElementById("nota3").value
+    const mediaTrimestral = parseInt( ( parseFloat(primeiraNota) + parseFloat(segundaNota) + parseFloat(terceiraNota) ) / 3)
+    console.log(mediaTrimestral);
+    let mediaTri = document.getElementById('media-tri')
+    mediaTri.innerText = mediaTrimestral
+    return mediaTrimestral
 }
 
-mediaTrimestral()
 
-let mediaDoPrimeiroTrimestre
-let mediaDoSegudoTrimestre
-let mediadoTerceiroTrimestre
+/**/
+/**/
+/**/
 
+
+/* função de botão para calcular a média Anual*/
+let botaoDeCalcularMediaAnual = document.getElementById('calc-media-anual')
+botaoDeCalcularMediaAnual.addEventListener('click', () => {
+    mediaAnual()
+})
+
+/* Função para calcular média Anual*/
 function mediaAnual() {
-    const mediaAnual = parseInt((mediaDoPrimeiroTrimestre + mediaDoSegudoTrimestre + mediaDoSegudoTrimestre) / 3)
-    console.log(mediaAnual)
+    let mediaDoPrimeiroTrimestre = document.getElementById("media1").value
+    let mediaDoSegudoTrimestre = document.getElementById("media2").value
+    let mediaDoTerceiroTrimestre = document.getElementById("media3").value
+    const mediaAnual = parseInt( ( parseFloat(mediaDoPrimeiroTrimestre) + parseFloat(mediaDoSegudoTrimestre) + parseFloat(mediaDoTerceiroTrimestre) ) / 3)
+    console.log(mediaAnual);
+    let mediaAno = document.getElementById('media-ano')
+    mediaAno.innerText = mediaAnual
+    return mediaAnual
 }
-
-mediaAnual()
