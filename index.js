87dbcfd91b9fd19947dbabc538bad5c7d6fed5e4
Kauê Martins <kauemm16@gmail.com>
2023-09-10 17:24:37 -0300

    adição da função de criar e excluir inputs

--- index.js
+++ index.js
@@ -1,56 +1,7 @@
-/* função de botão para calcular a média trimestral*/
-    let botaoDeCalcularMediaTrimestral = document.getElementById('calc-media-tri')
-    botaoDeCalcularMediaTrimestral.addEventListener('click', () => {
-        mediaTrimestral()
-            /*Toastify*/
-        Toastify({
-            text: "Média gerada",
-            duration: 1000,
-            position: "right",
-            gravity: "bottom",
-        }).showToast()
-    })
-
-/* Função para calcular média Trimestral*/
-function mediaTrimestral() {
-    let notasDeAvaliacoes = document.getElementsByClassName("nota")
-    console.log(notasDeAvaliacoes);
-    let nota1 = notasDeAvaliacoes[0].value
-    console.log(nota1)
-    let nota2 = notasDeAvaliacoes[1].value
-    console.log(nota2)
-    const mediaTrimestral = parseInt( (parseFloat(nota1) + parseFloat(nota2)) / notasDeAvaliacoes.length)
-    console.log(mediaTrimestral);
-    let mediaTri = document.getElementById('media-tri')
-    mediaTri.innerText = mediaTrimestral
-    if(nota1 && nota2 === undefined || null) {
-        return 0
-    }
-    return mediaTrimestral
-}
-
-/*Função para cirar novo input*/
- let botaoDeCriarInputs = document.getElementById('create')
- botaoDeCriarInputs.addEventListener('click', () => {
-    criarInput()
- })
-
-function criarInput() {
-    let caixaDeNotas = document.getElementsByClassName("caixa-de-notas")
-    let input = document.createElement("input")
-    input.classList.add('in-put', 'nota')
-    input.value = 0
-    caixaDeNotas.appendChild(input)   
-}
-/**/
-/**/
-/**/
-
-
-/* função de botão para calcular a média Anual*/
-let botaoDeCalcularMediaAnual = document.getElementById('calc-media-anual')
-botaoDeCalcularMediaAnual.addEventListener('click', () => {
-    mediaAnual()
+/* Evento de calcular Média*/
+let botaoDeCalcularMediaTrimestral = document.getElementById('calc-media-tri')
+botaoDeCalcularMediaTrimestral.addEventListener('click', () => {
+    mediaTrimestral()
     /*Toastify*/
     Toastify({
         text: "Média gerada",
@@ -60,14 +11,64 @@ botaoDeCalcularMediaAnual.addEventListener('click', () => {
     }).showToast()
 })
 
-/* Função para calcular média Anual*/
-function mediaAnual() {
-    let mediaDoPrimeiroTrimestre = document.getElementById("media1").value
-    let mediaDoSegudoTrimestre = document.getElementById("media2").value
-    let mediaDoTerceiroTrimestre = document.getElementById("media3").value
-    const mediaAnual = parseInt( ( parseFloat(mediaDoPrimeiroTrimestre) + parseFloat(mediaDoSegudoTrimestre) + parseFloat(mediaDoTerceiroTrimestre) ) / 3)
-    console.log(mediaAnual);
-    let mediaAno = document.getElementById('media-ano')
-    mediaAno.innerText = mediaAnual
-    return mediaAnual
+/* Função para calcular média Trimestral*/
+function mediaTrimestral() {
+    let notasDeAvaliacoes = document.getElementsByClassName("nota");
+    let soma = 0;
+  
+    for (let i = 0; i < notasDeAvaliacoes.length; i++) {
+      soma += parseFloat(notasDeAvaliacoes[i].value) || 0;
+    }
+  
+    const mediaTrimestral = (soma / notasDeAvaliacoes.length).toFixed(2);
+  
+    let mediaTri = document.getElementById('media-tri');
+    mediaTri.innerText = mediaTrimestral;
+  }
+
+/*Evento de criar nova caixa de nota*/
+let botaoDeCriarInputs = document.getElementById('create')
+botaoDeCriarInputs.addEventListener('click', () => {
+    criarInput()
+        /*Toastify*/
+        Toastify({
+            text: "Caixa Criada!",
+            duration: 2000,
+            position: "right",
+            gravity: "bottom",
+            style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+              },
+        }).showToast()
+})
+
+function criarInput() {
+    let notas = document.getElementsByClassName('nota')
+    if (notas.length < 6) {
+        let caixaDeInputs = document.getElementById("caixa-de-inputs")
+        let input = document.createElement('input')
+        input.value = 0
+        input.classList.add('in-put', 'nota')
+        caixaDeInputs.appendChild(input)
+    }
 }
+
+/*Evento de excluir caixa de nota*/
+let botaoDeExcluirInputs = document.getElementById('delete')
+botaoDeExcluirInputs.addEventListener('click', () => {
+    let InputDeNota = document.getElementsByClassName('nota')
+    if (InputDeNota.length > 2) {
+        InputDeNota[0].remove()
+    }
+        /*Toastify*/
+        Toastify({
+            text: "Caixa Excluida!",
+            duration: 1000,
+            position: "right",
+            gravity: "bottom",
+            style: {
+                background: "linear-gradient(to right, #d1301b , #ff0000)"
+              },
+        }).showToast()
+})
+
